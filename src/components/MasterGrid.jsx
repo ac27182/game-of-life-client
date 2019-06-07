7aadc280f030ce4868899e577ca86ced6297e819
ac27182 <a.cameron177@gmail.com>
2019-06-07 14:40:39 +0100

    finished experimenting with canvas sape adding

--- src/components/MasterGrid.jsx
+++ src/components/MasterGrid.jsx
@@ -4,7 +4,9 @@ import './MasterGrid.css'
 export class MasterGrid extends Component {
 	state = {
 		canvasHeight: '2048',
-		canvasWidth: '2048'
+		canvasWidth: '2048',
+		offset: [0,0],
+		isDown: false
 	}
 	
 	gridGen = (m) => {
@@ -17,20 +19,64 @@ export class MasterGrid extends Component {
 				ctx.stroke()
 			}				
 		}
-		// console.log(window.devicePixelRatio)
-		// console.log(ctx)
+	}
+
+	handleClick = (e) => {
+		const grid = this.refs.grid
+		const gridContainer = this.refs.gridContainer
+		const ctx = this.refs.grid.getContext('2d')
+
+		console.log('absolute click from left', e.clientX - (gridContainer.clientWidth - grid.clientWidth)/2)
+		console.log('absolute click from top', e.clientY - (gridContainer.clientHeight - grid.clientHeight)/2)
+		// absolute click from left and right
+		const coordinates = [
+			e.clientX - (gridContainer.clientWidth - grid.clientWidth)/2,
+			e.clientY - (gridContainer.clientHeight - grid.clientHeight)/2
+		]
+
+		ctx.fillStyle = '#00FFFF'
+		ctx.lineWidth = 0.25;
+		ctx.beginPath();
+		ctx.arc(coordinates[0], coordinates[1], 7, 0, 2*Math.PI)
+		ctx.fill()
+		ctx.stroke()
+
 	}
 
 	render() {
 		return (
-			<div className='grid'>
-				<canvas ref='grid' height={this.state.canvasHeight} width={this.state.canvasWidth}/>
+			<div className='grid-container' ref='gridContainer'>
+				<canvas ref='grid' height={this.state.canvasHeight} width={this.state.canvasWidth} onClick={(e) => this.handleClick(e)}/>
 			</div>
 		)
 	}
 	
 	componentDidMount() {
-		this.gridGen(7)
+		const grid = this.refs.grid
+		this.gridGen()
+		// grid.addEventListener('mousedown', (e) => {
+		// 	this.setState({
+		// 		isDown: true,
+		// 		offset: [
+		// 			grid.offsetLeft - e.clientX,
+		// 			grid.offsetTop - e.clientY
+		// 		]
+		// 	})
+		// }, true)
+
+		// grid.addEventListener('mouseup', (e) => {
+		// 	// e.preventDefault();
+		// 	this.setState({isDown: false})
+		// }, true)
+
+		// grid.addEventListener('mousemove', (event) => {
+		// 	event.preventDefault()
+		// 	if (this.state.isDown) {
+		// 		// grid.style.x = `${event.clientX + this.state.offset[0]}px`
+		// 		// grid.style.top = `${event.clientY + this.state.offset[1]}px`
+		// 		grid.style.left = '-50px'
+		// 	}
+		// }, true)
 	}
 
 	
