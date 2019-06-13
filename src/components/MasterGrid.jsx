ba85dc48bdf6862cdf7063c7a3451ee912577246
ac27182 <a.cameron177@gmail.com>
2019-06-13 23:01:23 +0100

    handleing r001 r002 server responses

--- src/components/MasterGrid.jsx
+++ src/components/MasterGrid.jsx
@@ -34,14 +34,15 @@ export class MasterGrid extends Component {
 			console.log('websocket not operational.')
 		}
 		connection.onmessage = event => {
-			const { MessageType, Payload } = JSON.parse(event.data)
-
-			switch (MessageType) {
-				case 'T001':
-					this.updateGrid(Payload)
+			const { code, payload } = JSON.parse(event.data)
+			console.log(payload.length / 12)
+			console.log(payload)
+			switch (code) {
+				case 'r002':
+					this.updateGrid(payload)
 					break
-				case 'T002':
-					this.drawNode(Payload)
+				case 'r001':
+					this.updateGrid(payload)
 					break
 				default:
 					break
@@ -51,15 +52,21 @@ export class MasterGrid extends Component {
 
 	/// updateGrid updates the life nodes on the grid
 	updateGrid = nodes => {
+		console.log('updating grid')
 		const ctx = this.refs.grid.getContext('2d')
 		const t = c => (Number(c) + 0.5) * 16
 		ctx.clearRect(0, 0, 2048, 2048)
 		this.gridGen()
 
+		const g = {}
+
 		for (let i = 0; i < nodes.length; i += 12) {
 			const x = t(Number(nodes.substring(i, i + 3)))
 			const y = t(Number(nodes.substring(i + 3, i + 6)))
 			const hex = `#${nodes.substring(i + 6, i + 12)}`
+
+			g[`${x}${y}`] = hex
+
 			ctx.fillStyle = hex
 			ctx.lineWidth = 0.25
 			ctx.beginPath()
@@ -67,6 +74,7 @@ export class MasterGrid extends Component {
 			ctx.fill()
 			ctx.stroke()
 		}
+		console.log(g)
 	}
 
 	drawNode = lifeNode => {
@@ -128,8 +136,12 @@ export class MasterGrid extends Component {
 		this.setState({ absX, absY })
 
 		// reduces absolute pixel number into circle number
+		const request = JSON.stringify({
+			code: 'c001',
+			payload: `${padAndReduce(absX)}${padAndReduce(absY)}${sessionColour}`,
+		})
 		console.log(padAndReduce(absX), padAndReduce(absY))
-		const request = `${padAndReduce(absX)}${padAndReduce(absY)}${sessionColour}`
+		console.log('request sent...')
 		connection.send(request)
 	}
 
